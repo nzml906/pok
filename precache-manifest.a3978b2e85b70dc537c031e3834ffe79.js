f9349359f77bfa30765eb04a3b8930c406543edf
Nazmul <grey000906@gmail.com>
2020-11-29 22:43:20 +0600

    deploy

---
+++ precache-manifest.a3978b2e85b70dc537c031e3834ffe79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b89cb6a3342f82047b",
+    "url": "/pok/css/app.f47e0d94.css"
+  },
+  {
+    "revision": "dc1dd8111f8c547399ad20c4175fa106",
+    "url": "/pok/index.html"
+  },
+  {
+    "revision": "a3b89cb6a3342f82047b",
+    "url": "/pok/js/app.d179575a.js"
+  },
+  {
+    "revision": "3eda8fb1c63a1e3fcde7",
+    "url": "/pok/js/chunk-vendors.bab76447.js"
+  },
+  {
+    "revision": "71ea336a0aef656a71ee84159677a1b2",
+    "url": "/pok/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pok/robots.txt"
+  }
+]);
